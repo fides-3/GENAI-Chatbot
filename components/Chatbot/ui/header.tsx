0f92426348266f03a09559212513f7903975164f
ntoiti <fidesluckia36@gmail.com>
2025-08-19 14:04:29 +0300

    added the header component

---
+++ components/Chatbot/ui/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-blue-600 text-white p-2  shadow-md rounded-t-md">
+      <div className="p-4 flex items-center justify-center">
+        <h1 className="font-semibold text-md">🤖 CHAT WITH OUR AI ASSISTANT</h1>
+      </div>
+    </header>
+  );
+}
+ 
